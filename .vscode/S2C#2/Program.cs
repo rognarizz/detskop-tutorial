3ad0aaf68dedea72b679e6155619345916aadd51
rognarizz <68757013+rognarizz@users.noreply.github.com>
2024-10-18 15:44:09 +0000

    add variables creation

---
+++ .vscode/S2C#2/Program.cs
@@ -0,0 +1,30 @@
+Console.WriteLine("Input your numeber for X coordinate: ");
+int numX = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input your numeber for Y coordinate: ");
+int numY = Convert.ToInt32(Console.ReadLine());
+
+if(numX > 0 && numY > 0)
+{
+    Console.WriteLine("Your number is in plane number 1");
+}
+
+if(numX < 0 && numY > 0)
+{
+    Console.WriteLine("Your number is in plane number 2");
+}
+
+if(numX < 0 && numY < 0)
+{
+    Console.WriteLine("Your number is in plane number 3");
+}
+
+if(numX > 0 && numY < 0)
+{
+    Console.WriteLine("Your number is in plane number 4");
+}
+
+if(numX == 0 && numY == 0)
+{
+    Console.WriteLine("Please, introduce another numbers!");
+}
